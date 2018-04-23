4ddb2728778f1ee96781f2d2de3b8b1761995849
Gabriel Ugalde <gabriel.10.delima@gmail.com>
2018-04-23 09:28:05 -0400

    mudança do ngrok e main

--- 2.rb
+++ 2.rb
@@ -6,13 +6,7 @@ require 'date'
 
 get '/' do
 
-	loop do
-		# "<p style='color: #fff; background-color: #{color}; width: 300px; text-align: center'>Server #{status} #{Time.now.strftime("%d/%m/%Y | %H:%M:%S")}</p>"
-		# sleep(5)
-		# return "#{check} - #{Time.now}"
-		"#{Time.now}"
-	end
-	# "#{check} - #{Time.now}"
+	"#{check} - #{Time.now}"
 
 end
 
@@ -26,7 +20,7 @@ end
 
 def check()
 
-	uri = URI.parse("http://8afa315b.ngrok.io/servstatus")
+	uri = URI.parse("https://8afa315b.ngrok.io/servstatus")
 	request = Net::HTTP::Post.new(uri)
 	request.content_type = "application/json"
 
